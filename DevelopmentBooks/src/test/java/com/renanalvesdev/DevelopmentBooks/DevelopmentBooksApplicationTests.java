ea38dcde46fb018d36eec8d5a8798d8027710920
Renan Alves <renanalvesemail@gmail.com>
2025-08-12 22:51:18 +0100

    initial commit

---
+++ DevelopmentBooks/src/test/java/com/renanalvesdev/DevelopmentBooks/DevelopmentBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanalvesdev.DevelopmentBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
