3bbde7e87576bb4b0983e451d2c925de408cc3d5
Renan Alves <renanalvesemail@gmail.com>
2025-08-14 23:38:44 +0100

    starting BasketControllerTest

---
+++ DevelopmentBooks/src/test/java/com/renanalvesdev/DevelopmentBooks/controller/BasketControllerTest.java
@@ -0,0 +1,50 @@
+package com.renanalvesdev.DevelopmentBooks.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.renanalvesdev.DevelopmentBooks.service.BasketService;
+
+@WebMvcTest(BasketController.class)
+public class BasketControllerTest {
+	 @Autowired
+	    private MockMvc mockMvc;
+
+	    @MockBean
+	    private BasketService basketService;
+	    
+	    @Autowired
+	    private ObjectMapper objectMapper;
+
+	    @Test
+	    void shouldCalculateTotalBasket() throws Exception {
+	        BasketDTO basket = new BasketDTO(List.of(
+	            new BasketItemDTO("Clean Coder", 1L),
+	            new BasketItemDTO("Clean Code", 1L)
+	        ));
+
+	        BigDecimal totalBasket = new BigDecimal("150.0");
+
+	        Mockito.when(basketService.calculateTotal(Mockito.any())).thenReturn(totalBasket);
+
+	        mockMvc.perform(post("/basket/calculate")
+	        	.contentType(MediaType.APPLICATION_JSON)
+	        	.content(objectMapper.writeValueAsString(basket))
+	        )
+	        .andExpect(status().isOk())
+	        .andExpect(content().string("150.0"));
+	    }
+}
