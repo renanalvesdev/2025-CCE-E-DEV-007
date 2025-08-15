cca3657aab96a4b0acf8a2ead765079e01ecd05c
Renan Alves <renanalvesemail@gmail.com>
2025-08-15 22:41:14 +0100

    Improvement to pass DTO as parameter

--- DevelopmentBooks/src/test/java/com/renanalvesdev/DevelopmentBooks/service/BasketServiceTest.java
+++ DevelopmentBooks/src/test/java/com/renanalvesdev/DevelopmentBooks/service/BasketServiceTest.java
@@ -6,12 +6,13 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.renanalvesdev.DevelopmentBooks.dto.BasketDTO;
+import com.renanalvesdev.DevelopmentBooks.dto.BasketItemDTO;
 import com.renanalvesdev.DevelopmentBooks.enums.Book;
 import com.renanalvesdev.DevelopmentBooks.model.BasketItem;
 
@@ -26,37 +27,39 @@ public class BasketServiceTest {
 	        return Stream.of(
 	            org.junit.jupiter.params.provider.Arguments.of(
 	             //5 per cent discount
-	             List.of(
-	            		 new BasketItem(1, Book.CLEAN_CODE),
-	            		 new BasketItem(1, Book.CLEAN_CODER)),new BigDecimal("95")),
+	            new BasketDTO( List.of(
+	            		 new BasketItemDTO(Book.CLEAN_CODE.getTitle(),1),
+	            		 new BasketItemDTO(Book.CLEAN_CODER.getTitle(), 1))),new BigDecimal("95")),
 	           //10 per cent discount
 	            org.junit.jupiter.params.provider.Arguments.of( 
-	            List.of(
-	            		 new BasketItem(1, Book.CLEAN_CODE),
-	            		 new BasketItem(1, Book.CLEAN_CODER),
-	            		 new BasketItem(1, Book.CLEAN_ARCHITECTURE)),new BigDecimal("135")),
+	            new BasketDTO(List.of(
+	            		 new BasketItemDTO(Book.CLEAN_CODE.getTitle(), 1),
+	            		 new BasketItemDTO(Book.CLEAN_CODER.getTitle(), 1),
+	            		 new BasketItemDTO(Book.CLEAN_ARCHITECTURE.getTitle(), 1))),new BigDecimal("135")),
 	           //20 per cent discount
-	            org.junit.jupiter.params.provider.Arguments.of( List.of(
-	            		 new BasketItem(1, Book.CLEAN_CODE),
-	            		 new BasketItem(1, Book.CLEAN_CODER),
-	            		 new BasketItem(1, Book.CLEAN_ARCHITECTURE),
-	            		 new BasketItem(1, Book.TEST_DRIVEN_DEVELOPMENT)),new BigDecimal("160")),
+	            org.junit.jupiter.params.provider.Arguments.of( 
+	            new BasketDTO( List.of(
+	            		 new BasketItemDTO(Book.CLEAN_CODE.getTitle(),1),
+	            		 new BasketItemDTO(Book.CLEAN_CODER.getTitle(),1),
+	            		 new BasketItemDTO(Book.CLEAN_ARCHITECTURE.getTitle(), 1),
+	            		 new BasketItemDTO(Book.TEST_DRIVEN_DEVELOPMENT.getTitle(),1))),new BigDecimal("160")),
 	    		 
 	           //25 per cent discount
-	            org.junit.jupiter.params.provider.Arguments.of( List.of(
-	            		 new BasketItem(1, Book.CLEAN_CODE),
-	            		 new BasketItem(1, Book.CLEAN_CODER),
-	            		 new BasketItem(1, Book.CLEAN_ARCHITECTURE),
-	            		 new BasketItem(1, Book.TEST_DRIVEN_DEVELOPMENT),
-	            		 new BasketItem(1, Book.WORKING_EFFECTIVELY_LEGACY_CODE)),new BigDecimal("187.5")
+	            org.junit.jupiter.params.provider.Arguments.of( 
+	            new BasketDTO(List.of(
+	            		 new BasketItemDTO(Book.CLEAN_CODE.getTitle(), 1),
+	            		 new BasketItemDTO(Book.CLEAN_CODER.getTitle(), 1),
+	            		 new BasketItemDTO(Book.CLEAN_ARCHITECTURE.getTitle(), 1),
+	            		 new BasketItemDTO(Book.TEST_DRIVEN_DEVELOPMENT.getTitle(), 1),
+	            		 new BasketItemDTO(Book.WORKING_EFFECTIVELY_LEGACY_CODE.getTitle(), 1))),new BigDecimal("187.5")
 	            
 	        ));
 	    }
 	 
 	@ParameterizedTest
 	@MethodSource("totalBasketTestCases")
-	void shouldCalculateTotalBasket(List<BasketItem> basketItens, BigDecimal total) {
-		BigDecimal totalBasket = basketService.calculateTotal(basketItens);
+	void shouldCalculateTotalBasket(BasketDTO basketDTO, BigDecimal total) {
+		BigDecimal totalBasket = basketService.calculateTotal(basketDTO);
 		assertEquals(0, totalBasket.compareTo(total));
       	
 	}
